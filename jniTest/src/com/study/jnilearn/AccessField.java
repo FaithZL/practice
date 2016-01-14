3ee66def6d4431cbf6e513524fb3c24687c341b0
SATAN_Z <249002031@qq.com>
2016-01-14 19:28:11 +0800

    ...

---
+++ jniTest/src/com/study/jnilearn/AccessField.java
@@ -0,0 +1,8 @@
+package com.study.jnilearn;
+import java.lang.System.*;
+
+public class AccessField{
+
+	public accessStaticField
+
+}
--- jniTest/src/com/study/jnilearn/ClassMethod.java
+++ jniTest/src/com/study/jnilearn/ClassMethod.java
@@ -3,13 +3,13 @@ package com.study.jnilearn;
 
 public class ClassMethod{
 
-    public static void callStaticMethod(String str , int val){
+    private static void callStaticMethod(String str , int val){
         
         System.out.println("type is " + str + ",num is " + val);
         
     }
     
-    public void callInstanceMethod(String str , int val){
+    private void callInstanceMethod(String str , int val){
         
         System.out.println("type is " + str + ",num is " + val);
         
