7fb055513054d6fa968256e2c5b3fe104f3ee595
SATAN_Z <249002031@qq.com>
2016-01-14 15:44:34 +0800

    test commit

--- jniTest/jni/com_study_jnilearn_HelloWorld.cpp
+++ jniTest/jni/com_study_jnilearn_HelloWorld.cpp
@@ -24,6 +24,8 @@ Java_com_study_jnilearn_HelloWorld_funInt(JNIEnv * jniEnv, jobject j, jint argIn
 JNIEXPORT jstring JNICALL 
 Java_com_study_jnilearn_HelloWorld_funStr(JNIEnv * env, jobject j, jstring argStr)
 {
+	jboolean isopy;
+	printf("isCopy:%d\n",isopy);
 	const char * cstr = env->GetStringUTFChars(argStr , 0);
 	cout<<"call by C++  "<<cstr<<endl;
 	char cap[128] = "language: ";
@@ -55,15 +57,39 @@ Java_com_study_jnilearn_HelloWorld_funIntArr(JNIEnv * env, jobject j, jintArray 
 	cout<<"call by C++  "<<endl;
 	jsize len = env->GetArrayLength(argIntArr);
 	jint * p = env->GetIntArrayElements(argIntArr , 0);
+	
+	cout<<"Method 1:"<<endl;
+
 	int sum = 0;
 	for (int i = 0; i < len; ++i)
 	{
 		cout<<"call by C++"<<p[i]<<endl;
 		sum = sum + p[i];
 	}
+	cout<<"the sum is "<<sum<<endl;
 	env->ReleaseIntArrayElements(argIntArr , p , 0);
+
+	cout<<"Method 2:"<<endl;
+	sum = 0;
+	//apply some buffer according by datatype and length
+	jint * pInt = (jint *)malloc(sizeof(jint) * len);
+	//initialize buffer
+	memset(pInt , 0 , sizeof(jint) * len);
+	//copy the argIntArr elements to the buffer. and  use SetIntArrayRegion(...) to change elements
+	env->GetIntArrayRegion(argIntArr , 0 , len , pInt);
+
+	for (int i = 0; i < len; ++i)
+	{
+		cout<<"call by C++  "<<pInt[i]<<endl;
+		sum = sum + pInt[i];
+	}
+	free(pInt);
+	cout<<"the sum is  "<<sum<<endl;
+	cout<<"C++ end"<<endl;
 	return sum;
 }
+
+
 
 
 
---
+++ jniTest/src/com/study/jnilearn/AccessMethod.java
@@ -0,0 +1,10 @@
+package com.study.jnilear;
+
+
+public class AccessMethod{
+	
+	public native static callJavaStaticMethod();
+
+	public native static callJavaInstanceMethod();
+
+}
